fd46cf9476c3f984d176c97f17b0a813db861ccb
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-09-09 22:13:37 +0200

    Home Controller

---
+++ src/main/java/com/galacticapp/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package com.galacticapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping({"/", "/api","/api/v1"})
+    public String message(){
+        return "index";
+
+    }
+
+}
--- src/main/resources/data.sql
+++
@@ -1,9 +0,0 @@
-drop table if exists planets;
-create table planets (
-    id int auto_increment primary key,
-    name varchar(10) not null
-);
-insert into planets (name) values ('Earth');
-insert into planets (name) values ('Mars');
-insert into planets (name) values ('Jupiter');
-insert into planets (name) values ('Venus');
