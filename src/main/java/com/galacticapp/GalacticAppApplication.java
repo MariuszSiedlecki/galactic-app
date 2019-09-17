427a4cb1d0dc40865f6fee5ca629e9861d1b49c0
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-09-17 13:33:15 +0200

    test user sql

--- src/main/java/com/galacticapp/GalacticAppApplication.java
+++ src/main/java/com/galacticapp/GalacticAppApplication.java
@@ -2,6 +2,9 @@ package com.galacticapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class GalacticAppApplication {
@@ -10,4 +13,8 @@ public class GalacticAppApplication {
         SpringApplication.run(GalacticAppApplication.class, args);
     }
 
+    @Bean
+    PasswordEncoder getPasswordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
