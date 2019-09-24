9c77d9ca1b1a71faff46ccf7eb9d4b3c2cd825de
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-09-24 10:53:39 +0200

    security

---
+++ src/main/java/com/galacticapp/security/Role.java
@@ -0,0 +1,30 @@
+package com.galacticapp.security;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "role")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "role_id")
+    private int roleId;
+    private String role;
+
+    @JsonIgnore
+    @ManyToMany(mappedBy = "roles")
+    private Set<UserApp> users = new HashSet<>();
+
+}
