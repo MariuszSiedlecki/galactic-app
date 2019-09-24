05a5a033760e8410175301fc643e38214ff79c51
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-09-25 00:22:02 +0200

    security

---
+++ src/main/java/com/galacticapp/security/CustomUserService.java
@@ -0,0 +1,41 @@
+package com.galacticapp.security;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+
+@Service
+public class CustomUserService implements UserDetailsService {
+
+    private UserAppRepository userAppRepository;
+    private PasswordEncoder passwordEncoder;
+
+    public CustomUserService(UserAppRepository userAppRepository, PasswordEncoder passwordEncoder) {
+        this.userAppRepository = userAppRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userAppRepository
+                .findUserAppByName(username)
+                .map(CustomUserDetails::new)
+                .orElseThrow(() -> new UsernameNotFoundException("user not find"));
+    }
+    public void saveUserApp(LoginUser loginUser){
+
+        userAppRepository.save(
+                UserApp
+                        .builder()
+                        .username(loginUser.getUsername())
+                        .password(passwordEncoder.encode(loginUser.getPassword()))
+                        .active(1)
+                        .roles(new HashSet<>())
+                        .build()
+        );
+    }
+}
