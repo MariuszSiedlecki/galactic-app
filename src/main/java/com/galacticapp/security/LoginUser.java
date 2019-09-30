d2e7eb7cc4441ebf5ff837b17b082ce6b39f6e49
Mariusz Siedlecki <siedlecki-mariusz@wp.pl>
2019-10-01 00:02:59 +0200

    new user in database

---
+++ src/main/java/com/galacticapp/security/LoginUser.java
@@ -0,0 +1,14 @@
+package com.galacticapp.security;
+
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Data
+public class LoginUser {
+    private String username;
+    private String password;
+}
